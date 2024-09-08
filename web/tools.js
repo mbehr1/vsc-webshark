cbf934d923913730ab1b7773ca773c613e7711b8
Matthias Behr <mbehr@mcbehr.de>
2024-09-08 08:10:39 +0200

    fix: add support for wireshark >= v4.1
    v4.1 introduced a change in the frame command for parameter
    'prev_frame' to be uinteger and not boolean.
    
    Added basic version detection and check for >= v4.1.
    Tested with v4.0.10, v4.2.7 and v4.4.0.
    Issue: about settings #29

--- web/tools.js
+++ web/tools.js
@@ -31,3 +31,28 @@ function sharkdResponse(res) {
         console.log('sharkdResponse err:' + err, JSON.stringify(res));
     }
 }
+
+/// compare a wireshark version string with a reference version string
+/// @param ws_vers: the version string to compare (e.g. "v4.0.10-0-gf...")
+/// @param ref_vers: the reference version string (e.g. "4.1.0")
+/// @return: -1 if ws_vers < ref_vers, 0 if ws_vers == ref_vers, 1 if ws_vers > ref_vers
+///
+/// Note: this function is not complete, it only compares the major, minor and patch version
+/// Note: it assumes the ws_vers string starts with the format "vX.Y.Z"
+/// todo: change to use semver lib (with webpack bundling...)
+function compare_ws_version(ws_vers, ref_vers){
+    if (!ws_vers.startsWith("v")) {
+        console.error("compare_ws_version: invalid ws_vers string", ws_vers, ref_vers);
+        return 0; // return 0 (= equal here...)
+    }
+    const ws_vers_parts = ws_vers.slice(1).split(".");
+    const ref_vers_parts = ref_vers.split(".");
+    for (let i = 0; i < ref_vers_parts.length; i++) {
+        let ws_part = parseInt(ws_vers_parts[i]);
+        if (isNaN(ws_part)) { ws_part = 0; }
+        const ref_part = parseInt(ref_vers_parts[i]);
+        if (ws_part < ref_part) { return -1; }
+        if (ws_part > ref_part) { return 1; }
+    }
+    return 0;
+}
